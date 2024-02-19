57a3ad0371ab42cd842448e2bdba301c542efac7
Eslam Abdelaziz <91923075+Eslam-Abdelaziz@users.noreply.github.com>
2024-02-20 00:49:02 +0200

    adding classes

--- Attendance Management System/Forms/Login.cs
+++ Attendance Management System/Forms/Login.cs
@@ -71,5 +71,109 @@ namespace Attendance_Management_System.Forms
         {
 
         }
+
+
+        private void pictureBoxShow_MouseHover(object sender, EventArgs e)
+        {
+            toolTip.SetToolTip(pictureBoxShow, "Show Password");
+        }
+
+        private void pictureBoxHide_MouseHover(object sender, EventArgs e)
+        {
+            toolTip.SetToolTip(pictureBoxShow, "Hide Password");
+        }
+
+        private void pictureBoxShow_Click(object sender, EventArgs e)
+        {
+            textBoxPassword.UseSystemPasswordChar = false;
+            pictureBoxShow.Hide();
+            pictureBoxHide.Show();
+        }
+
+        private void pictureBoxHide_click(object sender, EventArgs e)
+        {
+            textBoxPassword.UseSystemPasswordChar = true;
+            pictureBoxHide.Hide();
+            pictureBoxShow.Show();
+        }
+
+        private void LoginForm_Load(object sender, EventArgs e)
+        {
+            pictureBoxHide.Hide();
+            pictureBoxError.Hide();
+            labelError.Hide();
+            pictureBoxShow.Show();
+        }
+
+        private void pictureBoxMinimize_MouseHover(object sender, EventArgs e)
+        {
+            toolTip.SetToolTip(pictureBoxClose, "Minimize");
+        }
+
+        private void pictureBoxClose_MouseHover(object sender, EventArgs e)
+        {
+            toolTip.SetToolTip(pictureBoxClose, "Close");
+        }
+
+        private void buttonLogin_Click(object sender, EventArgs e)
+        {
+            string username = textBoxName.Text.Trim();
+            string pass = textBoxPassword.Text.Trim();
+            if (username != string.Empty && pass != string.Empty)
+            {
+                string role = isValidNamePass(username, pass);
+                if (role != string.Empty)
+                {
+                    MainForm mainForm = new MainForm();
+                    textBoxName.Clear();
+                    textBoxPassword.Clear();
+                    pictureBoxHide_click(sender, e);
+                    textBoxName.Focus();
+                    pictureBoxError.Hide();
+                    labelError.Hide();
+                    mainForm.ShowDialog();
+                }
+                else
+                {
+                    pictureBoxError.Show();
+                    labelError.Show();
+                }
+            }
+
+        }
+
+        #region handling Enter key
+        private void textBoxName_KeyUp(object sender, KeyEventArgs e)
+        {
+            if(e.KeyCode == Keys.Enter)
+                e.SuppressKeyPress = true;
+        }
+
+        private void textBoxName_KeyPress(object sender, KeyPressEventArgs e)
+        {
+            if(e.KeyChar == (char)Keys.Enter)
+            {
+                SelectNextControl(ActiveControl,true,true,true,true);
+                e.Handled = true;
+            }
+        }
+
+        private void textBoxPassword_KeyUp(object sender, KeyEventArgs e)
+        {
+            if(e.KeyCode == Keys.Enter)
+                e.SuppressKeyPress = true;
+
+        }
+
+        private void textBoxPassword_KeyPress(object sender, KeyPressEventArgs e)
+        {
+            if (e.KeyChar == (char)Keys.Enter)
+            {
+                buttonLogin.PerformClick();
+                e.Handled = true;
+            }
+
+        }
+        #endregion
     }
 }
