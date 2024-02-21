84c162a349df7ae35d0e179ec4a5a271692a8a4e
AhmedMahmoudMOD <ahmed.mahmoud.abdelnaby98@gmail.com>
2024-02-21 17:56:32 +0200

    working on class crud and ui

--- Attendance Management System/Forms/UserControlAddClass.Designer.cs
+++ Attendance Management System/Forms/UserControlAddClass.Designer.cs
@@ -28,18 +28,30 @@
         /// </summary>
         private void InitializeComponent()
         {
-            components = new System.ComponentModel.Container();
-            toolTip = new ToolTip(components);
             tabControlAddClass = new TabControl();
             tabPageAddClass = new TabPage();
-            tabPage2 = new TabPage();
-            labelAdd = new Label();
-            labelName = new Label();
-            textBoxName = new TextBox();
+            buttonAdd = new Button();
+            labelTeacher = new Label();
+            comboBoxTeacher = new ComboBox();
             textBoxID = new TextBox();
             labelID = new Label();
+            textBoxName = new TextBox();
+            labelName = new Label();
+            labelAdd = new Label();
+            tabPageUp = new TabPage();
+            buttonDelete = new Button();
+            buttonFind = new Button();
+            button1 = new Button();
+            label1 = new Label();
+            comboBoxUpTeacher = new ComboBox();
+            textBox1 = new TextBox();
+            label2 = new Label();
+            textBox2 = new TextBox();
+            label3 = new Label();
+            labelUp = new Label();
             tabControlAddClass.SuspendLayout();
             tabPageAddClass.SuspendLayout();
+            tabPageUp.SuspendLayout();
             SuspendLayout();
             // 
             // tabControlAddClass
@@ -47,7 +59,7 @@
             tabControlAddClass.Alignment = TabAlignment.Bottom;
             tabControlAddClass.Anchor = AnchorStyles.None;
             tabControlAddClass.Controls.Add(tabPageAddClass);
-            tabControlAddClass.Controls.Add(tabPage2);
+            tabControlAddClass.Controls.Add(tabPageUp);
             tabControlAddClass.Location = new Point(0, 0);
             tabControlAddClass.Name = "tabControlAddClass";
             tabControlAddClass.SelectedIndex = 0;
@@ -56,6 +68,9 @@
             // 
             // tabPageAddClass
             // 
+            tabPageAddClass.Controls.Add(buttonAdd);
+            tabPageAddClass.Controls.Add(labelTeacher);
+            tabPageAddClass.Controls.Add(comboBoxTeacher);
             tabPageAddClass.Controls.Add(textBoxID);
             tabPageAddClass.Controls.Add(labelID);
             tabPageAddClass.Controls.Add(textBoxName);
@@ -69,43 +84,37 @@
             tabPageAddClass.Text = "Add Class";
             tabPageAddClass.UseVisualStyleBackColor = true;
             // 
-            // tabPage2
+            // buttonAdd
             // 
-            tabPage2.Location = new Point(4, 4);
-            tabPage2.Name = "tabPage2";
-            tabPage2.Padding = new Padding(3);
-            tabPage2.Size = new Size(934, 468);
-            tabPage2.TabIndex = 1;
-            tabPage2.Text = "tabPage2";
-            tabPage2.UseVisualStyleBackColor = true;
+            buttonAdd.BackColor = Color.DarkSlateBlue;
+            buttonAdd.FlatStyle = FlatStyle.Flat;
+            buttonAdd.Font = new Font("Segoe UI", 12F, FontStyle.Bold);
+            buttonAdd.ForeColor = Color.White;
+            buttonAdd.Location = new Point(525, 263);
+            buttonAdd.Name = "buttonAdd";
+            buttonAdd.Size = new Size(179, 47);
+            buttonAdd.TabIndex = 7;
+            buttonAdd.Text = "Add";
+            buttonAdd.UseVisualStyleBackColor = false;
+            buttonAdd.Click += buttonAdd_Click;
             // 
-            // labelAdd
+            // labelTeacher
             // 
-            labelAdd.AutoSize = true;
-            labelAdd.Font = new Font("Segoe UI", 9F, FontStyle.Bold);
-            labelAdd.Location = new Point(6, 3);
-            labelAdd.Name = "labelAdd";
-            labelAdd.Size = new Size(77, 20);
-            labelAdd.TabIndex = 0;
-            labelAdd.Text = "Add Class";
+            labelTeacher.AutoSize = true;
+            labelTeacher.Font = new Font("Segoe UI", 9F, FontStyle.Bold);
+            labelTeacher.Location = new Point(80, 177);
+            labelTeacher.Name = "labelTeacher";
+            labelTeacher.Size = new Size(63, 20);
+            labelTeacher.TabIndex = 6;
+            labelTeacher.Text = "Teacher";
             // 
-            // labelName
+            // comboBoxTeacher
             // 
-            labelName.AutoSize = true;
-            labelName.Font = new Font("Segoe UI", 9F, FontStyle.Bold);
-            labelName.Location = new Point(80, 90);
-            labelName.Name = "labelName";
-            labelName.Size = new Size(51, 20);
-            labelName.TabIndex = 1;
-            labelName.Text = "Name";
-            // 
-            // textBoxName
-            // 
-            textBoxName.BorderStyle = BorderStyle.FixedSingle;
-            textBoxName.Location = new Point(80, 125);
-            textBoxName.Name = "textBoxName";
-            textBoxName.Size = new Size(210, 27);
-            textBoxName.TabIndex = 2;
+            comboBoxTeacher.FormattingEnabled = true;
+            comboBoxTeacher.Location = new Point(80, 210);
+            comboBoxTeacher.Name = "comboBoxTeacher";
+            comboBoxTeacher.Size = new Size(210, 28);
+            comboBoxTeacher.TabIndex = 5;
             // 
             // textBoxID
             // 
@@ -114,7 +123,6 @@
             textBoxID.Name = "textBoxID";
             textBoxID.Size = new Size(210, 27);
             textBoxID.TabIndex = 4;
-            textBoxID.TextChanged += textBox1_TextChanged;
             // 
             // labelID
             // 
@@ -126,6 +134,157 @@
             labelID.TabIndex = 3;
             labelID.Text = "ID";
             // 
+            // textBoxName
+            // 
+            textBoxName.BorderStyle = BorderStyle.FixedSingle;
+            textBoxName.Location = new Point(80, 125);
+            textBoxName.Name = "textBoxName";
+            textBoxName.Size = new Size(210, 27);
+            textBoxName.TabIndex = 2;
+            // 
+            // labelName
+            // 
+            labelName.AutoSize = true;
+            labelName.Font = new Font("Segoe UI", 9F, FontStyle.Bold);
+            labelName.Location = new Point(80, 90);
+            labelName.Name = "labelName";
+            labelName.Size = new Size(51, 20);
+            labelName.TabIndex = 1;
+            labelName.Text = "Name";
+            // 
+            // labelAdd
+            // 
+            labelAdd.AutoSize = true;
+            labelAdd.Font = new Font("Segoe UI", 9F, FontStyle.Bold);
+            labelAdd.Location = new Point(6, 3);
+            labelAdd.Name = "labelAdd";
+            labelAdd.Size = new Size(77, 20);
+            labelAdd.TabIndex = 0;
+            labelAdd.Text = "Add Class";
+            // 
+            // tabPageUp
+            // 
+            tabPageUp.Controls.Add(buttonDelete);
+            tabPageUp.Controls.Add(buttonFind);
+            tabPageUp.Controls.Add(button1);
+            tabPageUp.Controls.Add(label1);
+            tabPageUp.Controls.Add(comboBoxUpTeacher);
+            tabPageUp.Controls.Add(textBox1);
+            tabPageUp.Controls.Add(label2);
+            tabPageUp.Controls.Add(textBox2);
+            tabPageUp.Controls.Add(label3);
+            tabPageUp.Controls.Add(labelUp);
+            tabPageUp.Location = new Point(4, 4);
+            tabPageUp.Name = "tabPageUp";
+            tabPageUp.Padding = new Padding(3);
+            tabPageUp.Size = new Size(934, 468);
+            tabPageUp.TabIndex = 1;
+            tabPageUp.Text = "Update Class";
+            tabPageUp.UseVisualStyleBackColor = true;
+            // 
+            // buttonDelete
+            // 
+            buttonDelete.BackColor = Color.OrangeRed;
+            buttonDelete.FlatStyle = FlatStyle.Flat;
+            buttonDelete.Font = new Font("Segoe UI", 12F, FontStyle.Bold);
+            buttonDelete.ForeColor = Color.White;
+            buttonDelete.Location = new Point(669, 370);
+            buttonDelete.Name = "buttonDelete";
+            buttonDelete.Size = new Size(179, 47);
+            buttonDelete.TabIndex = 17;
+            buttonDelete.Text = "Delete";
+            buttonDelete.UseVisualStyleBackColor = false;
+            // 
+            // buttonFind
+            // 
+            buttonFind.BackColor = Color.DarkSlateBlue;
+            buttonFind.FlatStyle = FlatStyle.Flat;
+            buttonFind.Font = new Font("Segoe UI", 12F, FontStyle.Bold);
+            buttonFind.ForeColor = Color.White;
+            buttonFind.Location = new Point(387, 114);
+            buttonFind.Name = "buttonFind";
+            buttonFind.Size = new Size(182, 41);
+            buttonFind.TabIndex = 16;
+            buttonFind.Text = "Find";
+            buttonFind.UseVisualStyleBackColor = false;
+            // 
+            // button1
+            // 
+            button1.BackColor = Color.DarkSlateBlue;
+            button1.FlatStyle = FlatStyle.Flat;
+            button1.Font = new Font("Segoe UI", 12F, FontStyle.Bold);
+            button1.ForeColor = Color.White;
+            button1.Location = new Point(484, 370);
+            button1.Name = "button1";
+            button1.Size = new Size(179, 47);
+            button1.TabIndex = 15;
+            button1.Text = "Add";
+            button1.UseVisualStyleBackColor = false;
+            // 
+            // label1
+            // 
+            label1.AutoSize = true;
+            label1.Font = new Font("Segoe UI", 9F, FontStyle.Bold);
+            label1.Location = new Point(94, 238);
+            label1.Name = "label1";
+            label1.Size = new Size(63, 20);
+            label1.TabIndex = 14;
+            label1.Text = "Teacher";
+            // 
+            // comboBoxUpTeacher
+            // 
+            comboBoxUpTeacher.FormattingEnabled = true;
+            comboBoxUpTeacher.Location = new Point(94, 282);
+            comboBoxUpTeacher.Name = "comboBoxUpTeacher";
+            comboBoxUpTeacher.Size = new Size(210, 28);
+            comboBoxUpTeacher.TabIndex = 13;
+            // 
+            // textBox1
+            // 
+            textBox1.BorderStyle = BorderStyle.FixedSingle;
+            textBox1.Location = new Point(94, 127);
+            textBox1.Name = "textBox1";
+            textBox1.Size = new Size(210, 27);
+            textBox1.TabIndex = 12;
+            // 
+            // label2
+            // 
+            label2.AutoSize = true;
+            label2.Font = new Font("Segoe UI", 9F, FontStyle.Bold);
+            label2.Location = new Point(94, 93);
+            label2.Name = "label2";
+            label2.Size = new Size(25, 20);
+            label2.TabIndex = 11;
+            label2.Text = "ID";
+            // 
+            // textBox2
+            // 
+            textBox2.BorderStyle = BorderStyle.FixedSingle;
+            textBox2.Location = new Point(374, 282);
+            textBox2.Name = "textBox2";
+            textBox2.Size = new Size(210, 27);
+            textBox2.TabIndex = 10;
+            // 
+            // label3
+            // 
+            label3.AutoSize = true;
+            label3.Font = new Font("Segoe UI", 9F, FontStyle.Bold);
+            label3.Location = new Point(374, 238);
+            label3.Name = "label3";
+            label3.Size = new Size(51, 20);
+            label3.TabIndex = 9;
+            label3.Text = "Name";
+            // 
+            // labelUp
+            // 
+            labelUp.AutoSize = true;
+            labelUp.Font = new Font("Segoe UI", 9F, FontStyle.Bold);
+            labelUp.Location = new Point(6, 3);
+            labelUp.Name = "labelUp";
+            labelUp.Size = new Size(99, 20);
+            labelUp.TabIndex = 8;
+            labelUp.Text = "Update Class";
+            // 
             // UserControlAddClass
             // 
             AutoScaleDimensions = new SizeF(8F, 20F);
@@ -133,9 +292,12 @@
             Controls.Add(tabControlAddClass);
             Name = "UserControlAddClass";
             Size = new Size(942, 501);
+            Load += UserControlAddClass_Load;
             tabControlAddClass.ResumeLayout(false);
             tabPageAddClass.ResumeLayout(false);
             tabPageAddClass.PerformLayout();
+            tabPageUp.ResumeLayout(false);
+            tabPageUp.PerformLayout();
             ResumeLayout(false);
         }
 
@@ -144,11 +306,24 @@
         private ToolTip toolTip;
         private TabControl tabControlAddClass;
         private TabPage tabPageAddClass;
-        private TabPage tabPage2;
+        private TabPage tabPageUp;
         private Label labelName;
         private Label labelAdd;
         private TextBox textBoxName;
         private TextBox textBoxID;
         private Label labelID;
+        private Button buttonAdd;
+        private Label labelTeacher;
+        private ComboBox comboBoxTeacher;
+        private Button buttonDelete;
+        private Button buttonFind;
+        private Button button1;
+        private Label label1;
+        private ComboBox comboBoxUpTeacher;
+        private TextBox textBox1;
+        private Label label2;
+        private TextBox textBox2;
+        private Label label3;
+        private Label labelUp;
     }
 }
--- Attendance Management System/Forms/UserControlAddClass.cs
+++ Attendance Management System/Forms/UserControlAddClass.cs
@@ -7,19 +7,163 @@ using System.Linq;
 using System.Text;
 using System.Threading.Tasks;
 using System.Windows.Forms;
+using System.Xml;
+using System.Xml.Linq;
 
 namespace Attendance_Management_System.Forms
 {
     public partial class UserControlAddClass : UserControl
     {
+        private XDocument classesDocument;
+        private string classesFilePath = @"C:\Iti\C#XML\Attendance_Management_system\Attendance Management System\Data\classes.xml";
+        private string usersFilePath = @"C:\Iti\C#XML\Attendance_Management_system\Attendance Management System\Data\users.xml";
+        private string schemaFilePath = @"C:\Iti\C#XML\Attendance_Management_system\Attendance Management System\Data\classes_schema.xsd";
         public UserControlAddClass()
         {
             InitializeComponent();
         }
 
-        private void textBox1_TextChanged(object sender, EventArgs e)
+        private void PopulateTeacherComboBox()
+        {
+            // Load the users.xml file
+            XmlDocument doc = new XmlDocument();
+            doc.Load(usersFilePath);
+
+            // Get all "Class" nodes
+            XmlNodeList teacherNodes = doc.SelectNodes("//Teacher/Name");
+
+            // Extract teacher names and filter out duplicates
+            HashSet<string> teacherNames = new HashSet<string>();
+            foreach (XmlNode node in teacherNodes)
+            {
+                teacherNames.Add(node.InnerText);
+            }
+
+            // Add unique class names to the combo box
+            comboBoxTeacher.Items.AddRange(teacherNames.ToArray());
+            comboBoxUpTeacher.Items.AddRange(teacherNames.ToArray());
+            comboBoxTeacher.SelectedIndex = 0;
+            comboBoxUpTeacher.SelectedIndex = 0;
+        }
+
+        private void AddClass()
+        {
+            // Get student data from form controls
+            string name = textBoxName.Text;
+            int id;
+            if (!int.TryParse(textBoxID.Text, out id))
+            {
+                MessageBox.Show("Invalid ID. Please enter a valid numeric ID.");
+                return;
+            }
+            
+            string teacherName = comboBoxTeacher.SelectedItem.ToString();
+
+            if (!IsIdUnique(id))
+            {
+                MessageBox.Show("ID already exists. Please enter a unique ID.");
+                return;
+            }
+
+
+            // Validate student data against XML schema
+            if (!ValidateClassData(name, id ,teacherName))
+            {
+                MessageBox.Show("Class data is not valid.");
+                return;
+            }
+
+            // Write student data to users.xml file
+            WriteClassToXml(name, id, teacherName);
+
+            MessageBox.Show("Student added successfully!");
+            textBoxID.Clear();
+            textBoxName.Clear();
+
+        }
+
+        private bool IsIdUnique(int id)
+        {
+            XmlDocument doc = new XmlDocument();
+            doc.Load(classesFilePath);
+
+            XmlNodeList studentNodes = doc.SelectNodes("//Class");
+
+            foreach (XmlNode node in studentNodes)
+            {
+                int existingId = int.Parse(node.SelectSingleNode("ID").InnerText);
+
+                if (existingId == id)
+                {
+                    return false; // ID already exists
+                }
+            }
+
+            return true; // ID is unique
+        }
+
+        private bool ValidateClassData(string name, int id, string teacherName)
+        {
+            try
+            {
+                XmlReaderSettings settings = new XmlReaderSettings();
+                settings.Schemas.Add(null, schemaFilePath);
+                settings.ValidationType = ValidationType.Schema;
+
+                using (XmlReader reader = XmlReader.Create(new StringReader("<Class><ID>" + id + "</ID><Name>" + name + "</Name><Teacher>" + teacherName + "</Teacher></Class>"), settings))
+                {
+                    while (reader.Read()) { }
+                }
+
+                return true;
+            }
+            catch (Exception ex)
+            {
+                MessageBox.Show("XML validation error: " + ex.Message);
+                return false;
+            }
+        }
+
+        private void WriteClassToXml(string name, int id, string teacherName)
+        {
+            XmlDocument doc = new XmlDocument();
+            doc.Load(classesFilePath);
+
+            XmlElement root = doc.DocumentElement;
+
+            // Create a new class node
+            XmlElement classNode = doc.CreateElement("Class");
+
+            XmlElement idNode = doc.CreateElement("ID");
+            idNode.InnerText = id.ToString();
+            classNode.AppendChild(idNode);
+
+            XmlElement nameNode = doc.CreateElement("Name");
+            nameNode.InnerText = name;
+            classNode.AppendChild(nameNode);
+
+
+            XmlElement teacherNode = doc.CreateElement("Class");
+            teacherNode.InnerText = teacherName;
+            classNode.AppendChild(classNode);
+
+            // Append the student node to the root
+            root.AppendChild(classNode);
+
+            // Save the XML document
+            doc.Save(classesFilePath);
+        }
+
+
+
+        private void buttonAdd_Click(object sender, EventArgs e)
         {
 
+        }
+
+        private void UserControlAddClass_Load(object sender, EventArgs e)
+        {
+            PopulateTeacherComboBox();
         }
     }
 }
